3f8a9d204a841d4268aff338fcf7282969c2b1e2
SteveTaylor1970 <ts19700620@gmail.com>
2026-09-23 12:49:18 -0400

    add error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,24 +5,35 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
-    
-    formatted_response = json.loads(response.text)
-    #print(formatted_response)
-    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
 
     response_dict = {
-        'anger' : anger_score,
-        'disgust' : disgust_score,
-        'fear' : fear_score,
-        'joy' : joy_score,
-        'sadness' : sadness_score
+        'anger' : 'None',
+        'disgust' : 'None',
+        'fear' : 'None',
+        'joy' : 'None',
+        'sadness' : 'None',
+        'dominant_emotion' : 'None'
     }
-    dominant_key = max(response_dict, key=response_dict.get)
-    response_dict['dominant_emotion'] = dominant_key 
+
+    if response.status_code == 200 :
+        formatted_response = json.loads(response.text)
+        
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+        response_dict = {
+            'anger' : anger_score,
+            'disgust' : disgust_score,
+            'fear' : fear_score,
+            'joy' : joy_score,
+            'sadness' : sadness_score
+        }
+        dominant_key = max(response_dict, key=response_dict.get)
+        response_dict['dominant_emotion'] = dominant_key 
+    
     return response_dict
    
   
